7b76df3a35a2fa78c9f45873a43392ff153f7986
foddom520 <115153461+foddom520@users.noreply.github.com>
2025-11-03 10:04:06 +0100

    StartOnBackend

---
+++ Backend/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const mysql = require('mysql2');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createPool({
+    host: process.env.DB_Host,
+    user: process.env.DB_User,
+    password: process.env.DB_Password,
+    database: process.env.DB_Name,
+    port: process.env.DB_Port
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
