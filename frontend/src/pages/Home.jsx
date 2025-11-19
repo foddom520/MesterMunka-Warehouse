8e08e618e944ff64b0b32464065be4eb527b7493
retkesbegre <115552479+retkesbegre@users.noreply.github.com>
2025-11-19 18:40:33 +0100

    Az Home page javítása

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,26 +1,39 @@
-import Card from 'react-bootstrap/Card';
+import { useEffect, useState } from "react";
+import Card from "react-bootstrap/Card";
 
 function Home() {
+  const [auctions, setAuctions] = useState([]);
+
+  useEffect(() => {
+    // Példa API hívás
+    fetch("http://localhost:5000/api/auctions") // <-- saját API végpont
+      .then((res) => res.json())
+      .then((data) => setAuctions(data))
+      .catch((err) => console.error("Hiba az adatok lekérésekor:", err));
+  }, []);
+
   return (
-    <>
-      <Card>
-        <Card.Img variant="top" src="holder.js/100px180" />
-        <Card.Body>
-          <Card.Text>
-            Itt van az adott árverés adatai
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      <br />
-      <Card>
-        <Card.Body>
-          <Card.Text>
-            Itt van az adott árverés adatai
-          </Card.Text>
-        </Card.Body>
-        <Card.Img variant="bottom" src="holder.js/100px180" />
-      </Card>
-    </>
+    <div>
+      {/* Árverések száma */}
+      {auctions.length > 0 ? (
+        <h4>Összesen {auctions.length} árverés található</h4>
+      ) : (
+        <h4>Nincs jelenleg árverés</h4>
+      )}
+
+      {/* Lista megjelenítése */}
+      {auctions.map((auction, index) => (
+        <Card key={index} className="mb-3">
+          {auction.image && (
+            <Card.Img variant="top" src={auction.image} alt={auction.title} />
+          )}
+          <Card.Body>
+            <Card.Title>{auction.title}</Card.Title>
+            <Card.Text>{auction.description}</Card.Text>
+          </Card.Body>
+        </Card>
+      ))}
+    </div>
   );
 }
 
