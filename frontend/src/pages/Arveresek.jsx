15a8002fe6f704bd7f9c76ca0f63c628ba5f86f8
foddom520 <115153461+foddom520@users.noreply.github.com>
2025-11-21 10:32:55 +0100

    home-better

--- frontend/src/pages/Arveresek.jsx
+++ frontend/src/pages/Arveresek.jsx
@@ -1,15 +1,26 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Button, Row, Col, Card } from "react-bootstrap";
-import '../styles/HomePage.css';
+import backgroundimage from '../kepek/HomePageBackGround.png';
 
 const Arveresek = () => {
   return (
     <div>
       <div className="text-center py-5 background-Image-custom">
         <Container>
-          <h1>Árverések</h1>
-          <p className="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio maxime suscipit consequuntur, libero placeat aspernatur excepturi repellendus dicta. Reiciendis magnam cupiditate nam saepe similique voluptates, ipsum natus pariatur nulla omnis.</p>
+            <h1>Árverések</h1>
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={backgroundimage} />
+                <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                    Some quick example text to build on the card title and make up the
+                    bulk of the card's content.
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+            </Card>
+
         </Container>
       </div>
     </div>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,14 +2,15 @@ import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Button, Row, Col, Card } from "react-bootstrap";
 import '../styles/HomePage.css';
+import backgroundimage from '../kepek/HomePageBackGround.png';
 
 function App() {
   return (
     <>
       <div className="text-center py-5 background-Image-custom">
-        <Container>
+        <Container className="text-white">
           <h1>Bid & Lock</h1>
-          <p className="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio maxime suscipit consequuntur, libero placeat aspernatur excepturi repellendus dicta. Reiciendis magnam cupiditate nam saepe similique voluptates, ipsum natus pariatur nulla omnis.</p>
+          <p className="lead">Welcome to the auction platform where you can bid and lock your deals!</p>
         </Container>
       </div>
     </>
